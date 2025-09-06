7eb7f19766b45bd0295b2610ab088e96afb64556
Aditya S <aditya10wasd@gmail.com>
2025-09-06 19:33:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.92%) - LeetSync

--- 90-subsets-ii/subsets-ii.py
+++ 90-subsets-ii/subsets-ii.py
@@ -1,7 +1,7 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         res = []
-        nums.sort()
+        nums.sort() # when handling duplicates always sort
         subset = []
 
         def dfs(i):
@@ -16,7 +16,7 @@ class Solution:
 
             #rejecting nums[i]
             subset.pop()
-            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]: #for duplicates
                 i += 1
             dfs(i+1)
         dfs(0)
