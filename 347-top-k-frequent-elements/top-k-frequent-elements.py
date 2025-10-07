f08dfddc5e3a7786d8acdc330532583b1d57f041
Aditya S <aditya10wasd@gmail.com>
2025-10-07 12:43:09 +0530

    Time: 10 ms (25.66%) | Memory: 24.6 MB (7.84%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,30 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # lemme create a hashmap that stores element:frequency
+        hashmap = {}
+        for n in nums:
+            hashmap[n] = hashmap.get(n,0) + 1
+        
+
+        # lemme create a bucket for each frequency 
+        # 0  1  2  3  4  5  6. <--- frequency
+        #[] [] [] [] [] []  [] <---- bucket
+        nlen = len(nums)
+        buckets = [[] for _ in range(nlen+1)]
+        
+
+        # now we have to store element inside the [] corresponding to its frequency
+        for element,frequency in hashmap.items():
+            buckets[frequency].append(element)
+        
+        # since its top K elements, lets start from right end
+        result = []
+        for bucket in buckets[::-1]:
+            result.extend(bucket)
+            if len(result) == k:
+                return result
+
+
+    
+
+        
