1058b4554c9800b9e3abe46656adfe5dda8700be
Aditya S <aditya10wasd@gmail.com>
2025-12-07 11:09:59 +0530

    Time: 3 ms (83.44%) | Memory: 19.4 MB (51.30%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,12 +1,29 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = {}
+        # optimized approach: Boyer–Moore Voting Algorithm
+
+        count,res = 0,0
+        for n in nums:
+            if count ==0:
+                res = n
+            
+            count+= (1 if res == n else -1)
+        
+        return res
+        
+        
+        
+        
+        
+        
+        
+        """hashmap = {}
 
         for i in range(len(nums)):
             hashmap[nums[i]] = hashmap.get(nums[i],0)+1
         
         for key,value in hashmap.items():
             if value > len(nums)//2:
-                return key
+                return key"""
 
 
