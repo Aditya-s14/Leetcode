ac9dd1378871697744d24822fa3e35e4991c5dd4
Aditya S <aditya10wasd@gmail.com>
2026-01-14 19:32:06 +0530

    Time: 59 ms (7.66%) | Memory: 19.4 MB (10.53%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = []
+
+        for num in nums1:
+            found = False
+
+            for i in range(len(nums2)):
+                if nums2[i] == num:
+                    j = i + 1
+                    while j < len(nums2):
+                        if nums2[j] > num:
+                            res.append(nums2[j])
+                            found = True
+                            break
+                        j += 1
+                    break
+
+            if not found:
+                res.append(-1)
+
+        return res
