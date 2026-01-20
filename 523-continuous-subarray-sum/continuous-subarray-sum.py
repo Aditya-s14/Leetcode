6f2a0cdd1cbde06e764926e8a593f64c41959c69
Aditya S <aditya10wasd@gmail.com>
2026-01-20 14:47:57 +0530

    Time: 57 ms (26.95%) | Memory: 39.7 MB (17.45%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.py
+++ 523-continuous-subarray-sum/continuous-subarray-sum.py
@@ -1,18 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
 
-        hashmap = {0:-1}
+        hashmap = {0: -1}   # remainder -> earliest index
         prefix = 0
-        maxlen = 0
-        for i in range(len(nums)):
-            prefix+=nums[i]
-            rem = prefix%k
-            if rem in hashmap:
-                maxlen = max(maxlen,i-hashmap[rem])
-            
-            if rem not in hashmap:
-                hashmap[rem] = i
-            
-        return (maxlen>=2)
 
-        
+        for i in range(len(nums)):
+            prefix += nums[i]          
+            rem = prefix % k           # remainder
+
+            if rem in hashmap:
+                if i - hashmap[rem] >= 2:
+                    return True
+            else:
+                hashmap[rem] = i       # store rem not prefix
+
+        return False
