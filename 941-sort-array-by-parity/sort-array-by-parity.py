a0279b8a08074a76f05ccbd26d80182d9c56fd23
Aditya S <aditya10wasd@gmail.com>
2026-01-31 16:44:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (23.16%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.py
+++ 941-sort-array-by-parity/sort-array-by-parity.py
@@ -5,6 +5,6 @@ class Solution:
             if nums[right]%2 == 0:
                 nums[left],nums[right] = nums[right],nums[left]
                 left+=1
-            right+=1
+
         return nums
         
