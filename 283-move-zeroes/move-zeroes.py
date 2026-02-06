6ef3ac118e32793ca94c9af78433d43e5db27fcc
Aditya S <aditya10wasd@gmail.com>
2026-02-06 11:59:43 +0530

    Time: 1 ms (88.23%) | Memory: 20.7 MB (14.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,11 +1,15 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         left = 0  # pointer to place next non-zero
+        right= 0
+        while right<len(nums):
+            if nums[right]!=0:
+                nums[left],nums[right] = nums[right],nums[left]
+                left+=1
+                right+=1
+            else:
+                right+=1
 
-        for right in range(1, len(nums)):
-            if nums[left] == 0 and nums[right] != 0:
-                nums[left], nums[right] = nums[right], nums[left]
             
-            # if nums[left] is non-zero, move left forward
-            if nums[left] != 0:
-                left += 1
+
+
