31bf8c0fc6e454fe8bc33ad4a6a5a9e2426fb4df
Aditya S <aditya10wasd@gmail.com>
2026-02-08 11:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.06%) - LeetSync

---
+++ 376-wiggle-subsequence/wiggle-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return len(nums)
+
+        prev_diff = 0
+        count = 1
+
+        for i in range(1, len(nums)):
+            diff = nums[i] - nums[i - 1]
+
+            # sign change detected
+            if (diff > 0 and prev_diff <= 0) or (diff < 0 and prev_diff >= 0):
+                count += 1
+                prev_diff = diff
+
+        return count
